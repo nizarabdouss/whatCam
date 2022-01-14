c36b6ef4244924e7c9efb3b7f2c900a9a62a7d91
Nizar <nizarabdouss@gmail.com>
2022-01-14 12:18:52 -0500

    first commit

---
+++ whatCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatCam
+//
+//  Created by anonym on 2022-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
