8c90a57f37362e1fa21f94daf4a9013f4cff02ae
nizarabdouss <nizarabdouss@gmail.com>
2022-01-15 17:40:29 -0500

    Added input device and ouput layers/layout

--- whatCam/ViewController.swift
+++ whatCam/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
+        // Create Camera Device
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else{ return }
+        guard let input =  try? AVCaptureDeviceInput(device: captureDevice) else {return}
+        
+
+        captureSession.addInput(input)
+        captureSession.startRunning()
+
+        // Outputs
+
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+
+        view.layer.addSublayer(previewLayer)
+
+        previewLayer.frame = view.frame
+
+
     }
 
 
